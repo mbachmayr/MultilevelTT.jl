05f2a380d6cdee7e8fb21dbb2d19e1ff66b754b9
Markus Bachmayr <bachmayr@ins.uni-bonn.de>
2018-05-21 10:10:21 +0200

    Package setup

---
+++ src/MultilevelTT.jl
@@ -0,0 +1,7 @@
+#--------------------------------------------------------------
+# MultilevelTT package
+# example code for https://arxiv.org/abs/1802.09062
+#--------------------------------------------------------------
+module MultilevelTT
+push!(LOAD_PATH, @__DIR__) # expose all other modules in this directory
+end # module
